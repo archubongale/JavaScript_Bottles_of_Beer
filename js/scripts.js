d15b39509c7d1dfc9b979707fab0ead0f8990b47
Archana Bongale <archubongale@gmail.com>
2015-09-20 00:47:42 -0700

    Added all the files

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 var bottleSong = function(number) {
   var song = [];
   var absoluteNumber = Math.abs(number);
-  for (var i =absoluteNumber; i > 0; i--) {
-    song.push( i + "bottles of beer on the wall,");
-    song.push( i + "bottles of beer.");
-    song.push( "Take one down and pass it around,");
+  for (var i =absoluteNumber; i >= 0; i--) {
 
-    if(i > 1){
-      song.push( (i-1) + "bottles on the wall.");
+    if(i > 0){
+      song.push( i + " bottles of beer on the wall,");
+      song.push( i + " bottles of beer.");
+      song.push( "Take one down and pass it around, ");
+
+      song.push( (i-1) + " bottles on the wall.");
     }else{
       song.push("No more bottles on the wall,");
-      song.push("no more bottles of beer.Go to store and but some more," + absoluteNumber + "bottles of beer on the wall.");
+      song.push("no more bottles of beer. Go to store and but some more," + absoluteNumber + "bottles of beer on the wall.");
     }
   }
   return song.join("");
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,18 @@
 
 describe('bottleSong', function() {
-  it("uses correct last line", function() {
-    expect(bottleSong(1)).to.equal("1bottles of beer on the wall,1bottles of beer.Take one down and pass it around,No more bottles on the wall,no more bottles of beer.Go to store and but some more,1bottles of beer on the wall.");
+  it("returns for zero number", function() {
+    expect(bottleSong(0)).to.equal("No more bottles on the wall,no more bottles of beer. Go to store and but some more,0bottles of beer on the wall.");
   });
 
-  it("converts the negative number to absolute number", function() {
-    expect(bottleSong(-1)).to.equal("1bottles of beer on the wall,1bottles of beer.Take one down and pass it around,No more bottles on the wall,no more bottles of beer.Go to store and but some more,1bottles of beer on the wall.");
+  it("uses correct last line for number 1", function() {
+    expect(bottleSong(1)).to.equal("1 bottles of beer on the wall,1 bottles of beer.Take one down and pass it around, 0 bottles on the wall.No more bottles on the wall,no more bottles of beer. Go to store and but some more,1bottles of beer on the wall.");
+  });
+
+  it("takes the absolute value of the number", function() {
+    expect(bottleSong(-1)).to.equal("1 bottles of beer on the wall,1 bottles of beer.Take one down and pass it around, 0 bottles on the wall.No more bottles on the wall,no more bottles of beer. Go to store and but some more,1bottles of beer on the wall.");
   });
 
   it("returns for 2 bottles", function() {
-    expect(bottleSong(2)).to.equal("2bottles of beer on the wall,2bottles of beer.Take one down and pass it around,1bottles on the wall.1bottles of beer on the wall,1bottles of beer.Take one down and pass it around,No more bottles on the wall,no more bottles of beer.Go to store and but some more,2bottles of beer on the wall.");
+    expect(bottleSong(2)).to.equal("2 bottles of beer on the wall,2 bottles of beer.Take one down and pass it around, 1 bottles on the wall.1 bottles of beer on the wall,1 bottles of beer.Take one down and pass it around, 0 bottles on the wall.No more bottles on the wall,no more bottles of beer. Go to store and but some more,2bottles of beer on the wall.");
   });
 });
