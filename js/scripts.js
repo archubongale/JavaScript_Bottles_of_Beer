68e5b6964a02d3fa1297e816429da0d7d0f69eac
archubongale <archubongale@gmail.com>
2015-09-20 00:06:49 -0700

    Added specs,index and scripts files

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,19 @@
-// var bottleBeer = function(number) {
-//   var bottles;
-//   var absoluteNumber = Math.abs(number);
-//   for (var i=absoluteNumber; i >=1; i--){
-//     if( i == 1){
-//       bottles = 'bottle';
-//     }else {
-//       bottles = 'bottles';
-//     }
-//     console.log(i +" "+bottles+ "of beer on the wall" );
-//     if (i<99){
-//       console.log(i +" "+bottles+ "of beer on the wall");
-//     }
-//     console.log(i+" "+bottles+" of beer.");
-//     console.log("Take one down.");
-//     console.log("Pass it around.");
-//     if(i == 1){
-//       console.log("No bottles of beer on the wall")
-//     }
-//   }
-//
-// };
-
 var bottleSong = function(number) {
   var song = [];
   var absoluteNumber = Math.abs(number);
   for (var i =absoluteNumber; i > 0; i--) {
-    song.push(i+ "bottles on the wall,");
-    song.push(i+ "bottles of beer.");
-    song.push("Take one down and pass it around.");
-    console.log(song);
-    debugger;
+    song.push( i + "bottles of beer on the wall,");
+    song.push( i + "bottles of beer.");
+    song.push( "Take one down and pass it around,");
+
     if(i > 1){
-      song.push((i-1)+ "bottles on the wall")
-    }else {
-      song.push("No more bottles on the wall")
+      song.push( (i-1) + "bottles on the wall.");
+    }else{
+      song.push("No more bottles on the wall,");
+      song.push("no more bottles of beer.Go to store and but some more," + absoluteNumber + "bottles of beer on the wall.");
     }
   }
-  return song;
+  return song.join("");
 };
 
 $(document).ready(function() {
@@ -44,7 +21,7 @@ $(document).ready(function() {
     var number= parseInt($("input#number").val());
     var result = bottleSong(number);
 
-      $(".result").append(song);
+      $(".output").text(result);
       $("#result").show();
      event.preventDefault();
   });
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,14 @@
 
-// dexcribe('bottleSong',function() {
-//   it("gives the deceremented value of the bottle", function() {
-//     expect(bottleSong(1).to.equal("No more bottles on the wall"));
-//   });
-// });
+describe('bottleSong', function() {
+  it("uses correct last line", function() {
+    expect(bottleSong(1)).to.equal("1bottles of beer on the wall,1bottles of beer.Take one down and pass it around,No more bottles on the wall,no more bottles of beer.Go to store and but some more,1bottles of beer on the wall.");
+  });
+
+  it("converts the negative number to absolute number", function() {
+    expect(bottleSong(-1)).to.equal("1bottles of beer on the wall,1bottles of beer.Take one down and pass it around,No more bottles on the wall,no more bottles of beer.Go to store and but some more,1bottles of beer on the wall.");
+  });
+
+  it("returns for 2 bottles", function() {
+    expect(bottleSong(2)).to.equal("2bottles of beer on the wall,2bottles of beer.Take one down and pass it around,1bottles on the wall.1bottles of beer on the wall,1bottles of beer.Take one down and pass it around,No more bottles on the wall,no more bottles of beer.Go to store and but some more,2bottles of beer on the wall.");
+  });
+});
